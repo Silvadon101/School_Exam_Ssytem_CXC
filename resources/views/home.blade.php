8fc2980a896df177166a319fa921b443ae1113d2
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2021-11-01 16:37:05 -0500

    Add Home livewire component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CXC | Home</title>
-    {{-- -----Tailwind Css----- --}}
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+    @livewireStyles
 </head>
 <body>
-    {{-- ------navbar------ --}}
-    @include('layouts.includes.nav')
 
-    
-    <div class="flex justify-center">
-        <h1 class="text-red-800 text-7xl">Welcome</h1>
-    </div>
+    <livewire:home-display />
+
+    @livewireScripts
 </body>
 </html>
---
+++ resources/views/livewire/home-display.blade.php
@@ -0,0 +1,11 @@
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+   
+    {{-- ------navbar------ --}}
+    @include('layouts.includes.nav')
+
+    <div class="flex justify-center">
+        <h1 class="text-green-400 text-7xl">Welcome</h1>
+    </div>
+
+
+
